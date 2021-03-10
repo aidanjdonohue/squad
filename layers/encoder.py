dcb00a09ce2d90202f01405a06889a5839e1984b
“blakejbrandon” <blakejbrandon@gmail.com>
2021-03-10 11:33:37 -0800

    Began adding additional models for self attention.

--- layers/encoder.py
+++ layers/encoder.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
+from self_attention import SelfAttMultiHeadLayer
 
 class HighwayEncoder(nn.Module):
     """Encode an input sequence using a highway network.
@@ -80,3 +81,55 @@ class RNNEncoder(nn.Module):
         x = F.dropout(x, self.drop_prob, self.training)
 
         return x
+
+class SelfAttEncoder(nn.Module):
+    """General-purpose layer for encoding a sequence using a bidirectional RNN.
+
+    Encoded output is the RNN's hidden state at each position, which
+    has shape `(batch_size, seq_len, hidden_size * 2)`.
+
+    Args:
+        input_size (int): Size of a single timestep in the input.
+        hidden_size (int): Size of the RNN hidden state.
+        num_layers (int): Number of layers of RNN cells to use.
+        drop_prob (float): Probability of zero-ing out activations.
+    """
+    def __init__(self,
+                 input_size,
+                 hidden_size,
+                 num_layers=6,
+                 output_dimension = 512,
+                 drop_prob=0.1):
+
+        super(SelfAttEncoder, self).__init__()
+        self.drop_prob = drop_prob
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers,
+                           batch_first=True,
+                           bidirectional=True,
+                           dropout=drop_prob if num_layers > 1 else 0.)
+        self.sublayer_one = SelfAttMultiHeadLayer(input_size, 0.1, 0.1, 8)
+        self.sublayer_two = SelfAttFeedForwardLayer()
+
+
+    def forward(self, x, lengths):
+        # Save original padded length for use by pad_packed_sequence
+        orig_len = x.size(1)
+
+        # Sort by length and pack sequence for RNN
+        lengths, sort_idx = lengths.sort(0, descending=True)
+        x = x[sort_idx]     # (batch_size, seq_len, input_size)
+        x = pack_padded_sequence(x, lengths, batch_first=True)
+
+        # Apply RNN
+        x, _ = self.rnn(x)  # (batch_size, seq_len, 2 * hidden_size)
+
+        # Unpack and reverse sort
+        x, _ = pad_packed_sequence(x, batch_first=True, total_length=orig_len)
+        _, unsort_idx = sort_idx.sort(0)
+        x = x[unsort_idx]   # (batch_size, seq_len, 2 * hidden_size)
+
+        # Apply dropout (RNN applies dropout after all but the last layer)
+        x = F.dropout(x, self.drop_prob, self.training)
+
+        return x
+
--- models.py
+++ models.py
@@ -11,6 +11,84 @@ import torch
 import torch.nn as nn
 
 
+class SelfAttModel(nn.module):
+    super(BiDAFself, self).__init__()
+
+    self.word_embd = embedding.WordEmbedding(word_vectors=word_vectors,
+                                             hidden_size=hidden_size,
+                                             drop_prob=drop_prob)
+    self.char_embd = embedding.CharEmbedding(char_vectors=char_vectors)
+    self.hwy = encoder.HighwayEncoder(2, 2*hidden_size)
+
+    self.enc = encoder.SelfAttEncoder(input_size=2*hidden_size,
+                                     hidden_size=hidden_size,
+                                     num_layers=1,
+                                     drop_prob=drop_prob)
+    
+    self.att = bidaf.BiDAFAttention(hidden_size=2*hidden_size,
+                                         drop_prob=drop_prob)
+
+    self.mod = encoder.RNNEncoder(input_size=8 * hidden_size,
+                                     hidden_size=hidden_size,
+                                     num_layers=2,
+                                     drop_prob=drop_prob)
+
+    self.out = bidaf.BiDAFOutput(hidden_size=hidden_size,
+                                      drop_prob=drop_prob)
+
+        def build_contextual_encoding(self, x_w, x_c, w_len, c_len):
+        char_embd = self.char_embd(x_c)
+
+        word_embd = self.word_embd(x_w)
+
+        #print(f'char_embd shape{char_embd.shape}')
+        #print(f'word_embd shape{word_embd.shape}')
+
+        embd = torch.cat((char_embd, word_embd), 2)
+
+        #print(f'cat_embd shape {embd.shape}')
+
+        embd = self.hwy(embd)
+        #print(f'hwy_embd shape {embd.shape}')
+        lens = w_len #+ c_len
+        #print(f'lens: {w_len} c_len: {c_len}')
+
+
+
+        encoding = self.enc(embd, lens)
+        
+        return encoding
+
+
+
+    # ctx_w, ctx_c, query_w, query_c
+    def forward(self, cw_idxs, qw_idxs, cc_idxs, qc_idxs):
+        cw_mask = torch.zeros_like(cw_idxs) != cw_idxs
+        qw_mask = torch.zeros_like(qw_idxs) != qw_idxs
+        cw_len, qw_len = cw_mask.sum(-1), qw_mask.sum(-1)
+
+        cc_mask = torch.zeros_like(cc_idxs) != cc_idxs
+        qc_mask = torch.zeros_like(qc_idxs) != qc_idxs
+        cc_len, qc_len = cc_mask.sum(-1), qc_mask.sum(-1)
+     
+        c_enc = self.build_contextual_encoding(cw_idxs, cc_idxs, cw_len, cc_len)
+        q_enc = self.build_contextual_encoding(qw_idxs, qc_idxs, qw_len, qc_len)
+
+        # hs = 2x
+        #print(f'context_encoder {c_enc.shape}')
+        #print(f'query_encoder {q_enc.shape}')
+        #print(f'Expecting batch, cw_len, 800')
+        att = self.att(c_enc, q_enc,
+                       cw_mask, qw_mask)    # (batch_size, c_len, 8 * hidden_size)
+        
+        #print(f'att shape {att.shape}')
+        mod = self.mod(att, cw_len)        # (batch_size, c_len, 2 * hidden_size)
+        
+        #print(f'mod shape {mod.shape}')
+        out = self.out(att, mod, cw_mask)  # 2 tensors, each (batch_size, c_len)
+
+        return out
+
 class BiDAFplus(nn.Module):
     # word_vectors -> word_embeddings
     # char_vectors -> char_embeddings
